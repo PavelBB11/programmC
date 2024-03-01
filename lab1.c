cb2008363ba5e0cb4c2775103d4d77258e01578f
PavelBB11 <159598061+PavelBB11@users.noreply.github.com>
2024-03-01 15:43:16 +1000

    lab1.c

--- lab1.c
+++ lab1.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-int main(int argc, char *argv[])
-{ 
-double a, b, c, x1, x2, d;
-a=5;
-b=6;
-c=1;
-d = b*b - 4*a*c;
-printf("d = %lf ",d);
-x1 = (-b + sqrt(d))/(2*a);
-x2 = (-b - sqrt(d))/(2*a);
-printf("x1=%lf x2=%lf" ,x1,x2);
-
-
+int main()
+{
+    double a, b, c, D=0, x1, x2;
+    int x;
+    do {
+    
+    printf("Введите коэффициент при a: ");
+    scanf("%lf", &a);
+    printf("Введите коэффициент при b: ");
+    scanf("%lf", &b);
+    printf("Введите коэффициент при c: ");
+    scanf("%lf", &c);
+    
+    D = b*b - 4*a*c;
+    printf("D = %lf\n", D);
+    
+    if (D>0) {
+        x1=(-b+sqrt(D))/(2*a);
+        x2=(-b-sqrt(D))/(2*a);
+    printf("x1=%lf\nx2=%lf\n", x1, x2);
+     
+    }
+    if (D==0) {
+        x1=(-b/(2*a));
+    }
+    if (D<0) {
+        printf("Корней нет\n");
+    }
+    printf("Для повторения работы программы введите 1:\n");
+    scanf("%d", &x); 
+    } while(x==1);
     return 0;
 }
