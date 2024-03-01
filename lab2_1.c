a9b8272923dc2c7abd851e5a6308c0635ec59f20
PavelBB11 <159598061+PavelBB11@users.noreply.github.com>
2024-03-01 16:19:42 +1000

    Update lab2_1.c

--- lab2_1.c
+++ lab2_1.c
@@ -5,7 +5,6 @@ int main() {
     double sum_main_diagonal = 0;
     double sum_secondary_diagonal = 0;
 
-    // Ввод значений массива с консоли
     printf("Введите значения массива:\n");
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
@@ -14,17 +13,14 @@ int main() {
         }
     }
 
-    // Вычисление суммы элементов на главной диагонали
     for (int i = 0; i < 3; i++) {
         sum_main_diagonal += array[i][i];
     }
 
-    // Вычисление суммы элементов на побочной диагонали
     for (int i = 0; i < 3; i++) {
         sum_secondary_diagonal += array[i][2 - i];
     }
 
-    // Вывод суммы элементов на главной и побочной диагоналях
     printf("Сумма элементов на главной диагонали: %lf\n", sum_main_diagonal);
     printf("Сумма элементов на побочной диагонали: %lf\n", sum_secondary_diagonal);
 
