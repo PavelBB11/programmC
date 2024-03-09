4e543d6e263860c98ca4002df46b49cada48fc36
PavelBB11 <159598061+PavelBB11@users.noreply.github.com>
2024-03-09 13:23:38 +1000

    Update triangle.c

--- triangle.c
+++ triangle.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <stdbool.h>
 double calculateP(double a, double b, double c) {
     return a + b + c;
 }
@@ -6,4 +7,12 @@ double calculateP(double a, double b, double c) {
 double calculateS(double a, double b, double c) {
     double p = calculateP(a, b, c) / 2;
     return sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+bool checkT(double a, double b, double c) {
+    if (a + b > c && a + c > b && b + c > a) {
+        return true;
+    } else {
+        return false;
+    }
 }
