b92a4481c4c8a8cb6d0febcefc68f81b1a60cdd2
PavelBB11 <159598061+PavelBB11@users.noreply.github.com>
2024-03-23 00:13:42 +1000

    Create lab6.c

---
+++ lab6.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    FILE *inputFile, *outputFile;
+    char firstName[50], lastName[50], middleName[50];
+    int year;
+
+    inputFile = fopen("people.txt", "w");
+    if (inputFile == NULL) {
+        printf("Ошибка при открытии файла.\n");
+        return 1;
+    }
+    fprintf(inputFile, "Иванов Петр Сергеевич 1975\n");
+    fprintf(inputFile, "Сидоров Николай Андреевич 1981\n");
+    fprintf(inputFile, "Воробьянинов Ипполит Матвеевич 1978\n");
+    // Добавить остальные записи с именами и годами рождения
+    fclose(inputFile);
+
+    inputFile = fopen("people.txt", "r");
+    outputFile = fopen("people_filtered.txt", "w");
+    if (inputFile == NULL || outputFile == NULL) {
+        printf("Ошибка при открытии файлов.\n");
+        return 1;
+    }
+    while (fscanf(inputFile, "%s %s %s %d", lastName, firstName, middleName, &year) != EOF) {
+        if (year > 1980) {
+            fprintf(outputFile, "%s %s %s %d\n", lastName, firstName, middleName, year);
+        }
+    }
+    fclose(inputFile);
+    fclose(outputFile);
+
+    return 0;
+}
